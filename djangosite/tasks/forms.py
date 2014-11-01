521020749aed8eaa206234911f16ee03d5dd93f2
euranium <mountrainier13@gmail.com>
2014-11-01 13:03:05 -0700

    made users and user creation

--- djangosite/tasks/forms.py
+++ djangosite/tasks/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from tasks.models import 
+#from tasks.models import
 from django.contrib.auth.models import Users
 
-class UserForm():
+class UserForm(forms.ModelForm):
+    username = forms.CharField(help_text ="Please enter a username")
+    first_name = forms.CharField(help_text ="Please enter your first name")
+    last_name = forms.CharField(help_text = "Please enter you last name")
+    email = forms.CharField(help_text = "Please enter your email")
+    password = forms.CharField(widget = forms.PasswordInput(), help_text = "Please enter you password")
 
-
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
--- djangosite/tasks/models.py
+++ djangosite/tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    #basic user model
+    def __unicode__(self):
+        return self.user.name
+
+
