cf085a53171573f3cac717acd85dd1bf2b0f0c68
wolfpack94 <benedea@students.wwu.edu>
2014-11-01 13:57:19 -0700

    Added urls.py and index.html

--- djangosite/tasks/views.py
+++ djangosite/tasks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 
-# Create your views here.
+def index(request):
+    context = RequestContext(request)
+    context_dict = {}
+#    if request.user.is_authenticated():
+#        user = str(request.user.get_full_name())
+#        context_dict['str_user']=user
+    return render_to_response('tasks/index.html', context_dict, context)
