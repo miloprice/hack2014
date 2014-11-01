52702d4c5324d2d78ad9342edf9e2711d7edce37
Milo Price <pricem9@students.wwu.edu>
2014-11-01 12:46:28 -0700

    Created models

--- djangosite/tasks/models.py
+++ djangosite/tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField()
+    assigned = models.DateTimeField()
+    due = models.DateTimeField()
+    tags = models.CharField()
+    priority = models.ForeignKey(Priority)
+    status = models.ForeignKey(Status)
+    size = models.ForeignKey(Size)
+    all_day = models.BooleanField()
+    repeat = models.BooleanField()
+    location = models.CharField()
+    desc = models.CharField()
+
+class Priority(models.Model):
+    name = models.CharField()
+    ranking = models.IntegerField()
+
+class Status(models.Model):
+    completed = models.IntegerField()
+    total = models.IntegerField()
+    qualitative = models.CharField()
+
+class Size(models.Model):
+    name = models.CharField()
+    ranking = models.IntegerField()
